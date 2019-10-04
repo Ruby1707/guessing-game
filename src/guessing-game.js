6fac27c116ce81b9ddd491bebb906ca6be9be1bc
Ruby1707 <53220616+Ruby1707@users.noreply.github.com>
2019-10-05 00:09:13 +0300

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+      this.Big=0;
+      this.Small=0;
+      this.Answer=0;
+    }
 
     setRange(min, max) {
-
+      this.Big=max;
+      this.Small=min;
     }
 
     guess() {
-
+      return this.new=Math.round((this.Big+this.Small)/2);
     }
 
     lower() {
-
+      this.setRange(this.Small,this.new)
     }
 
     greater() {
-
+      this.setRange(this.new,this.Big)
     }
 }
 
 module.exports = GuessingGame;
+
